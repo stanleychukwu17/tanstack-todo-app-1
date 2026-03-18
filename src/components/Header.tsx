e14368e09a56b0c144c9faee007e4b432b095d8c
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-03-18 11:51:14 +0100

    refactor(header): update title to "Todo List"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 
 export default function Header() {
   return (
-    <header className="">
-      TODO
+    <header className="text-4xl font-semibold pt-3.5 px-4">
+      Todo List
     </header>
   )
 }
