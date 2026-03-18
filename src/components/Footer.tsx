57e5c60f3b211eedffb6a412b03213f56a2d1f9d
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-03-18 11:54:08 +0100

    style(footer): hide footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const year = new Date().getFullYear()
 
   return (
-    <footer className="text-center">
+    <footer className="text-center hidden">
       <p className="text-sm">&copy; {year} Your name here. All rights reserved.</p>
     </footer>
   )
