529d9bd402b47de1db034c2ab143b4bf0de31981
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-03-18 11:48:31 +0100

    feat(routes): implement loader to fetch todos from server

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,27 @@
+import { db } from '#/db'
 import { createFileRoute } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
 
-export const Route = createFileRoute('/')({ component: App })
+const todoFromServer = createServerFn({ "method": "GET" }).handler(() => {
+  return db.query.todos.findMany()
+})
+
+export const Route = createFileRoute('/')({
+  loader: () => {
+    console.log(todoFromServer(), 'hi boy!')
+    return todoFromServer()
+  },
+  component: App
+})
 
 function App() {
+  const todos = Route.useLoaderData()
+  console.log(todos.length)
+
   return (
     <main className="page-wrap px-4 pb-8 pt-14">
       <h1>Todo</h1>
+
     </main>
   )
 }
