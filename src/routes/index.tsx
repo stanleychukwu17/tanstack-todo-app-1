bcb4b3df7693a743c4a76ac252be5261a7279654
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-03-20 17:47:40 +0100

    feat(routes): implement empty state with 'Add Todo' link and Plus icon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,16 @@
+import { Button } from '#/components/ui/button'
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '#/components/ui/empty'
 import { db } from '#/db'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start'
+import { ListTodo, Plus } from 'lucide-react'
 
 const todoFromServer = createServerFn({ "method": "GET" }).handler(() => {
   return db.query.todos.findMany()
@@ -8,7 +18,6 @@ const todoFromServer = createServerFn({ "method": "GET" }).handler(() => {
 
 export const Route = createFileRoute('/')({
   loader: () => {
-    console.log(todoFromServer(), 'hi boy!')
     return todoFromServer()
   },
   component: App
@@ -16,12 +25,52 @@ export const Route = createFileRoute('/')({
 
 function App() {
   const todos = Route.useLoaderData()
-  console.log(todos.length)
+  console.log(todos)
 
   return (
-    <main className="page-wrap px-4 pb-8 pt-14">
-      <h1>Todo</h1>
-
+    <main className="page-wrap px-4 py-4">
+      {
+        (todos.length === 0) ? (
+          <EmptyTodos />
+        ) : (
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.id}>{todo.title}</li>
+            ))}
+          </ul>
+        )
+      }
     </main>
+  )
+}
+
+function EmptyTodos() {
+  return (
+    <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <ListTodo />
+        </EmptyMedia>
+      </EmptyHeader>
+      <EmptyTitle>
+        No todos found
+      </EmptyTitle>
+      <EmptyDescription>
+        Get started by creating a new todo.
+      </EmptyDescription>
+      <EmptyContent>
+        <Button size="lg" className='px-4' asChild>
+          <Link to="/todo/new">
+            <Plus /> Add Todo
+          </Link>
+        </Button>
+      </EmptyContent>
+    </Empty>
+  )
+}
+
+function TodoList() {
+  return (
+    <>Hi</>
   )
 }
