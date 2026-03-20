43b1188d1b6413799a9f697679f41e66e3d0537d
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-03-20 17:47:09 +0100

    feat(ui): add basic Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 
 export default function Header() {
   return (
-    <header className="text-4xl font-semibold pt-3.5 px-4">
+    <header className="text-4xl font-semibold pt-3.5 px-4 text-center">
       Todo List
     </header>
   )
